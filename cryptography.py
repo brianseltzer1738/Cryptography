c8a0d000ffee8d0f86267ad4b9069fae310cb768
brianseltzer1738 <brianseltzer@hanovernorwichschools.org>
2017-04-24 14:22:20 -0400

    Updated from Brython Server: 4/24/2017 2:22:18 PM

--- cryptography.py
+++ cryptography.py
@@ -12,19 +12,7 @@ To get a character back from this numeric representation, get the character at t
 in the string (using associations[index]).
 MUST ENCRYPT AND DECRYPT 
 """
-"""
-cryptography.py
-Author: Sam
-Credit: none yet
-Assignment:
-Write and submit a program that encrypts and decrypts user data.
-See the detailed requirements at https://github.com/HHS-IntroProgramming/Cryptography/blob/master/README.md
-To get a character's numeric representation, find the index of that character in the string 
-(use the code associations.find(char) to get the index of char's first occurrence in associations)
-To get a character back from this numeric representation, get the character at that number's index 
-in the string (using associations[index]).
-MUST ENCRYPT AND DECRYPT 
-"""
+
 associations = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 repeatasoc="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 .,:;'\"/\\<>(){}[]-=_+?!"
 order=str(input("Enter e to encrypt, d to decrypt, or q to quit: "))
